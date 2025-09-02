138f58d87d57ac24e8e727d241a711216c421e2c
dxmv <dimitirjestepanovic1@gmail.com>
2025-09-02 20:30:14 +0200

    Display suggestions on frontend

--- webapp/src/api/suggestApi.ts
+++ webapp/src/api/suggestApi.ts
@@ -9,11 +9,11 @@ export const suggestApi = async (prefix: string) => {
     if (prefix === '' || prefix.length < 3 || prefix.includes('http') || prefix.includes('/')) {
         return []
     }
-    const res = await fetch(`${API_URL}/api/suggest`, {
+    const res = await fetch(`${API_URL}/api/suggest?prefix=${prefix}`, {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(prefix),
+        method: 'GET',
     })
     if (!res.ok) {
         throw new Error('Error: ' + res.statusText)
--- webapp/src/components/search-bar/index.tsx
+++ webapp/src/components/search-bar/index.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react"
+import { suggestApi } from "../../api/suggestApi"
 
-const SearchBar = ({className, value, handleChange}: {className: string, value: string, handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
-  
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+const SearchBar = ({className, resultClassName, value, handleChange}: {className: string, resultClassName?: string, value: string, handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
+  const [suggestions, setSuggestions] = useState<string[]>([])
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSuggestions([])
     handleChange(e)
     // get the redis suggestions here
+    try {
+      const suggestions = await suggestApi(e.target.value)
+      setSuggestions(suggestions)
+    } catch (error) {
+      console.error(error)
+    }
   }
   return (
+    <div className="relative">
     <input type="text" className={className} name="query" value={value} onChange={onChange} />
+    {suggestions.length > 0 && (
+      <div className={`absolute top-[100%] left-0 w-full bg-white`}>
+        {suggestions.map((suggestion) => (
+          <div  key={suggestion} className={`suggestion hover:cursor-pointer hover:bg-gray-100 border-b border-[#676767] ${resultClassName}`}>{suggestion}</div>
+        ))}
+      </div>
+    )}
+    </div>
   )
 }
 
